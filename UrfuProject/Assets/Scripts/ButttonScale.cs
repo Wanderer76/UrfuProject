5c250bb7dc111e87ae8c211b8a5c0a64bdae6eca
Wanderer76 <ateplinsky@mail.ru>
2021-03-15 15:02:37 +0500

    добавил сцену с меню и основные кнопки

---
+++ UrfuProject/Assets/Scripts/ButttonScale.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ButttonScale : MonoBehaviour
+{
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+        rectTransform.localScale = new Vector3(0.9f, 0.9f, 1);
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("OnMouseUp");
+        rectTransform.localScale = new Vector3(1, 1, 1);
+    }
+}
