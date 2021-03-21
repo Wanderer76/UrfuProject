03f3fa1986f5f2aa3da26c8a7926d9a473d06de3
Wanderer76 <ateplinsky@mail.ru>
2021-03-21 18:08:57 +0500

    Добавил главную игровую сцену
    Ввел игровую статистику, надо поработать над архитектурой получившегося, слишком много повторения кода

---
+++ UrfuProject/Assets/Scripts/GameStats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameStats
+{
+
+    public enum Scenes
+    {
+        Menu = 0,
+        MainScene,
+    }
+
+    public enum ScienceKoef
+    {
+        Math = 0,
+        Physics,
+        Biology,
+        Chemestry
+    }
+
+    public static int Money { get; set; } = 1024;
+
+    public static int SciencePoints { get; private set; } = 0;
+
+    public static int LaboratoryLevel { get; set; } = 2;
+
+    public static int QuestCompleted { get; }
+
+    public static List<string> CurrentQuestions { get; }
+
+    public static void AddSciencePoints(ScienceKoef koef)
+    {
+        switch (koef)
+        {
+            case ScienceKoef.Math:
+                SciencePoints += 30;
+                break;
+            case ScienceKoef.Physics:
+                SciencePoints += 100;
+                break;
+            case ScienceKoef.Biology:
+                SciencePoints += 130;
+                break;
+            case ScienceKoef.Chemestry:
+                SciencePoints += 200;
+                break;
+        }
+    }
+}
---
+++ UrfuProject/Assets/Scripts/MainSceneScripts/UIScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    private const string ScienceText = "Научные очки:";
+    private const string MoneyText = "Деньги:";
+
+    public GameObject panel;
+
+    public Text sciencePointsText;
+    public Text moneyText;
+
+    private void Awake()
+    {
+        UpdateStatisticUI();
+    }
+
+    private void UpdateStatisticUI()
+    {
+        sciencePointsText.text = $"{ScienceText}{GameStats.SciencePoints}";
+        moneyText.text = $"{MoneyText}{GameStats.Money}";
+    }
+    
+    public void OnMathBought(int count)
+    {
+        if (GameStats.Money >= count)
+            Upgrade(count, GameStats.ScienceKoef.Math);
+    }
+    public void OnPhysicsBought(int count)
+    {
+        if (GameStats.Money >= count)
+            Upgrade(count, GameStats.ScienceKoef.Physics);
+    }
+    public void OnBiologyBought(int count)
+    {
+        if (GameStats.Money >= count)
+            Upgrade(count, GameStats.ScienceKoef.Biology);
+    }
+    public void OnChemestryBought(int count)
+    {
+        if (GameStats.Money >= count)
+            Upgrade(count, GameStats.ScienceKoef.Chemestry);
+    }
+
+    private void Upgrade(int count, GameStats.ScienceKoef koef)
+    {
+        GameStats.Money -= count;
+        GameStats.AddSciencePoints(koef);
+        UpdateStatisticUI();
+    }
+
+    public void OnQuitClicked()
+    {
+        panel.SetActive(true);
+    }
+
+    public void Continue()
+    {
+        panel.SetActive(false);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene((int)GameStats.Scenes.Menu);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
